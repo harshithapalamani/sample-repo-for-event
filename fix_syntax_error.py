1aaf707fe940ef480cbba8ef1ea506d3a4f808f9
kasarapusuneetha <kasarapusuneetha@gmail.com>
2024-07-22 16:20:59 +0000

    debuug&fixed

--- fix_syntax_error.py
+++ fix_syntax_error.py
@@ -6,7 +6,7 @@
 def calculate_area(radius)
     pi = 3.14159
     return pi * radius ** 2
-
+answerjhdhfljdhglk
 # Correct the syntax error and explain:
 # The syntax error is _______________________________________________________
 
